14317429682109b15f4a69d4be1330d4e1fe2fb9
Jaime Villegas <jaimevillegas296@gmail.com>
2023-02-28 11:21:13 -0500

    Finish task 1

---
+++ string.test.js
@@ -0,0 +1,14 @@
+const string = require('./string.js');
+const evalString = 'jaime';
+
+test('Determine characters on a string', () => {
+  expect(string(evalString)).toBe(5);
+});
+
+test('Less than 10?', () => {
+  expect(string(evalString)).toBeLessThan(10);
+});
+
+test('At leat 1 character', () => {
+  expect(string(evalString)).toBeGreaterThan(1);
+});
